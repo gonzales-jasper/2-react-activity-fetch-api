30f8280ab2d042d58ea0c93a1df57684cf317c3a
jasper-gonzales <pro.japegonzales@gmail.com>
2026-08-22 10:02:51 +0800

    Add: Mapping Api with Search Functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import { useState, useEffect } from "react";
 import "./App.css";
+import DisplayUsers from "./DisplayUsers.jsx";
 
 function App() {
   const [data, setData] = useState();
   const [error, setError] = useState();
+  const [searchUser, setSearchUser] = useState("");
 
   useEffect(() => {
     async function fetchData() {
       try {
-        const res = await fetch("https://jsonplaceholder.typicode.com/users/1");
+        const res = await fetch("https://jsonplaceholder.typicode.com/users");
 
         if (res.status !== 200) {
           throw new Error("Failed to fetch data");
@@ -31,32 +33,26 @@ function App() {
     return <h1>Loading.....</h1>;
   }
 
+  const filteredList = data.filter((user) => {
+    return user.name.toLowerCase().includes(searchUser.toLowerCase());
+  });
   //------------display---------------
   return (
     <>
-      <div className="card">
-        <div className="card-header">
-          <div className="avatar">{data.name.charAt(0).toUpperCase()}</div>
-          <div className="name">
-            <h2>{data.name}</h2>
-            <p className="subtext">{`@${data.username}`}</p>
-          </div>
-        </div>
-        <div className="card-body">
-          <div className="contact-info">
-            <p className="blue-text">{data.email}</p>
-            <p>{data.phone}</p>
-            <p className="blue-text">{data.website}</p>
-          </div>
-
-          <div className="address">
-            <p>{`${data.address.street}, ${data.address.suite}, ${data.address.city}, ${data.address.zipcode}`}</p>
-          </div>
-        </div>
-
-        <div className="card-footer">
-          <p>{data.company.name}</p>
-          <p className="subtext">{data.company.catchPhrase}</p>
+      <div className="body">
+        <form>
+          <h1>Search User</h1>
+          <input
+            type="text"
+            placeholder="Search users..."
+            value={searchUser}
+            onChange={(e) => setSearchUser(e.target.value)}
+          />
+        </form>
+        <div className="container">
+          {filteredList.map((user) => (
+            <DisplayUsers data={user} />
+          ))}
         </div>
       </div>
     </>
---
+++ src/DisplayUsers.jsx
@@ -0,0 +1,31 @@
+const DisplayUsers = ({ data }) => {
+  return (
+    <div className="card">
+      <div className="card-header">
+        <div className="avatar">{data.name.charAt(0).toUpperCase()}</div>
+        <div className="name">
+          <h2>{data.name}</h2>
+          <p className="subtext">{`@${data.username}`}</p>
+        </div>
+      </div>
+      <div className="card-body">
+        <div className="contact-info">
+          <p className="blue-text">{data.email}</p>
+          <p>{data.phone}</p>
+          <p className="blue-text">{data.website}</p>
+        </div>
+
+        <div className="address">
+          <p>{`${data.address.street}, ${data.address.suite}, ${data.address.city}, ${data.address.zipcode}`}</p>
+        </div>
+      </div>
+
+      <div className="card-footer">
+        <p>{data.company.name}</p>
+        <p className="subtext">{data.company.catchPhrase}</p>
+      </div>
+    </div>
+  );
+};
+
+export default DisplayUsers;
